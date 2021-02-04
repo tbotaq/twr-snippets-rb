8452ec9b549049e1624943ebea6809e60bd41da6
tbotaq <tbotaq@gmail.com>
2021-02-04 12:31:26 +0900

    Fix the sort of collection and fetched tweets

--- spec/libs/fetch_user_tweets_spec.rb
+++ spec/libs/fetch_user_tweets_spec.rb
@@ -2,14 +2,14 @@ describe FetchUserTweets do
   describe '#call' do
     subject { instance.call(collection, min_id: min_id, max_id: max_id, batch_size: batch_size) }
     let(:instance) { described_class.new(twitter_rest_client) }
-    let(:collection) { 1.upto(3).map { |n| instance_double(Twitter::Tweet, n.to_s, id: n) } }
     let(:min_id) { 10 }
     let(:max_id) { 100 }
     let(:batch_size) { 100 }
 
     let(:twitter_rest_client) { instance_double(Twitter::REST::Client) }
-    context 'fetched no tweet' do
+    context 'API returns no tweet' do
       before { allow(twitter_rest_client).to receive(:user_timeline).and_return([]) }
+      let(:collection) { 1.upto(3).map { |n| instance_double(Twitter::Tweet, n.to_s, id: n) } }
       it 'should have made an API request with correct parameter' do
         subject
         expect(twitter_rest_client).to have_received(:user_timeline).with(
@@ -34,8 +34,9 @@ describe FetchUserTweets do
       end
     end
 
-    context 'fetched some tweets' do
-      let(:tweets) { 6.downto(4).map { |n| instance_double(Twitter::Tweet, n.to_s, id: n) } }
+    context 'API returns some tweets' do
+      let(:collection) { 6.downto(4).map { |n| instance_double(Twitter::Tweet, n.to_s, id: n) } }
+      let(:tweets) { 3.downto(1).map { |n| instance_double(Twitter::Tweet, n.to_s, id: n) } }
       before do
         # stub the first API request
         allow(twitter_rest_client).to receive(:user_timeline).with(
@@ -56,7 +57,7 @@ describe FetchUserTweets do
         # spy the #call method
         allow(instance).to receive(:call).and_call_original
       end
-      it 'makes an API request with correct parameter' do
+      it 'should have made an API request with correct parameter' do
         subject
         expect(twitter_rest_client).to have_received(:user_timeline).with(
           hash_including(
